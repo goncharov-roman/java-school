d4de8346eb38b50a2528882ebc9e95dcb07bb86a
Roman Goncharov <romg92@mail.ru>
2020-10-04 00:18:44 +0300

    fixed cipher test

--- lesson9/src/test/java/org/roman/cipher/CipherUtilsTest.java
+++ lesson9/src/test/java/org/roman/cipher/CipherUtilsTest.java
@@ -2,12 +2,15 @@ package org.roman.cipher;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CipherUtilsTest {
 
     @Test
     void testEncrypt() {
-        System.out.println(CipherUtils.encrypt("Hello, world!"));
+        String cryptogram = CipherUtils.encrypt("Hello, world!");
+        System.out.println(cryptogram);
 
-
+        assertTrue(cryptogram.length() > 0);
     }
 }
